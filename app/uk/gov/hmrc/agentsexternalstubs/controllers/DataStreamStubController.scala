17a35793c9bded17a6859c462672698f100b819c
Artur Opala <opala.artur@gmail.com>
2018-12-14 13:44:36 +0100

    change method of logging audit events

--- app/uk/gov/hmrc/agentsexternalstubs/controllers/DataStreamStubController.scala
+++ app/uk/gov/hmrc/agentsexternalstubs/controllers/DataStreamStubController.scala
@@ -1,7 +1,9 @@
 package uk.gov.hmrc.agentsexternalstubs.controllers
 
 import javax.inject.{Inject, Singleton}
+import play.api.Logger
 import play.api.libs.concurrent.ExecutionContextProvider
+import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent}
 import uk.gov.hmrc.play.bootstrap.controller.BaseController
 
@@ -13,7 +15,9 @@ class DataStreamStubController @Inject()(ecp: ExecutionContextProvider) extends 
   implicit val ec: ExecutionContext = ecp.get()
 
   val writeEvent: Action[AnyContent] = Action.async { implicit request =>
-    request.body.asJson.foreach(println)
+    request.body.asJson
+      .map(Json.prettyPrint)
+      .foreach(Logger(getClass).info(_))
     Future.successful(NoContent)
   }
 
