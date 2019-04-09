7fb89690b848866eba5b09a5ff4114fc10158b0c
Artur Opala <250927+arturopala@users.noreply.github.com>
2019-04-09 12:20:52 +0100

    do not fail mongo query if unit expected

--- app/uk/gov/hmrc/agentsexternalstubs/repository/MongoHelper.scala
+++ app/uk/gov/hmrc/agentsexternalstubs/repository/MongoHelper.scala
@@ -14,7 +14,7 @@ object MongoHelper {
   }
 
   val interpretWriteResultUnit: WriteResult => Future[Unit] = { r =>
-    if (!r.ok || r.code.isDefined || r.n == 0)
+    if (!r.ok || r.code.isDefined)
       Future.failed(WriteResult.lastError(r).getOrElse(new Exception(r.writeErrors.mkString(","))))
     else
       Future.successful(())
