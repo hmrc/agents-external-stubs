02d9f1e40f4eab640df66dd77fe010f44f54e40a
stevenmallen <42972200+stevenmallen@users.noreply.github.com>
2019-10-21 16:55:33 +0100

    [APB-4427][SJM] Add simple state management for PDV stub

--- app/uk/gov/hmrc/agentsexternalstubs/controllers/PersonalDetailsValidationController.scala
+++ app/uk/gov/hmrc/agentsexternalstubs/controllers/PersonalDetailsValidationController.scala
@@ -8,6 +8,7 @@ import play.api.mvc.{Action, AnyContent, ControllerComponents}
 import uk.gov.hmrc.agentsexternalstubs.models.{Generator, PersonalDetailsValidation, PersonalDetailsWithNino}
 import uk.gov.hmrc.play.bootstrap.controller.BackendController
 
+import scala.collection.mutable
 import scala.concurrent.ExecutionContext
 
 @Singleton
@@ -16,14 +17,25 @@ class PersonalDetailsValidationController @Inject()(cc: ControllerComponents)(im
 
   import uk.gov.hmrc.agentsexternalstubs.models.PersonalDetailsValidationFormat._
 
+  private val validationIds = mutable.Map[String, Boolean]()
+
   def get(id: String): Action[AnyContent] = Action { implicit request =>
     Ok(toJson(personalDetailsValidationRepository.get(id)))
   }
 
+  // associate a result (success/failure) with a validation id
+  def pdvResult(id: String, success: Boolean): Action[AnyContent] = Action {
+    validationIds(id) = success
+    Ok
+  }
+
   object personalDetailsValidationRepository {
     def get(id: String): PersonalDetailsValidation =
-      PersonalDetailsValidation
-        .successful(id, PersonalDetailsWithNino("Fred", "Bloggs", LocalDate.now(), Generator.ninoNoSpaces(id)))
+      if (validationIds.getOrElse(id, false))
+        PersonalDetailsValidation
+          .successful(id, PersonalDetailsWithNino("Fred", "Bloggs", LocalDate.now(), Generator.ninoNoSpaces(id)))
+      else
+        PersonalDetailsValidation.failed(id)
   }
 
 }
