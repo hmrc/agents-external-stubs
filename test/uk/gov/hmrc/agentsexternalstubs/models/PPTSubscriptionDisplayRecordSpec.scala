aaae21f74382457e7d45c68566408387458f96b1
Unmesh Malvankar <unmesh.malvankar1@digital.hmrc.gov.uk>
2022-03-08 18:27:07 +0000

    [APB-5739][UM] Fixes creation of customerDetails before storing PPTSubscriptionDisplayRecord

---
+++ test/uk/gov/hmrc/agentsexternalstubs/models/PPTSubscriptionDisplayRecordSpec.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentsexternalstubs.models
+
+import org.scalatest.Inspectors
+import uk.gov.hmrc.agentsexternalstubs.models.PPTSubscriptionDisplayRecord.LegalEntityDetails.CustomerDetails
+import uk.gov.hmrc.agentsexternalstubs.support.UnitSpec
+
+class PPTSubscriptionDisplayRecordSpec extends UnitSpec {
+
+  val seeds = "foeba".permutations.toSeq
+
+  "CustomerDetails" should {
+
+    "be generated correctly" in {
+      Inspectors.forAll(seeds) { seed: String =>
+        val entity = CustomerDetails.generate(seed)
+        entity.customerType match {
+          case "Individual" =>
+            entity.individualDetails shouldBe defined
+            entity.organisationDetails shouldBe None
+          case "Organisation" =>
+            entity.individualDetails shouldBe None
+            entity.organisationDetails shouldBe defined
+          case _ => fail(s"Unknown customer type ${entity.customerType}")
+        }
+      }
+    }
+
+  }
+
+}
