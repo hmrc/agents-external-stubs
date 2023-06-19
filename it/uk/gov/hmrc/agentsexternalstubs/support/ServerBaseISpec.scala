0079bdc2f026aaa3b9f453a420df181b4ae2f453
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-06-19 15:57:10 +0100

    APB-7219 [CS] update README.md

--- it/uk/gov/hmrc/agentsexternalstubs/support/ServerBaseISpec.scala
+++ it/uk/gov/hmrc/agentsexternalstubs/support/ServerBaseISpec.scala
@@ -1,14 +1,20 @@
 package uk.gov.hmrc.agentsexternalstubs.support
 
 import org.scalatest.BeforeAndAfterAll
-import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.time.{Milliseconds, Seconds, Span}
 import play.api.Application
 
 import scala.concurrent.ExecutionContext
 
 abstract class ServerBaseISpec
-    extends BaseISpec with BeforeAndAfterAll with ScalaFutures with JsonMatchers with WSResponseMatchers with MongoDB {
+    extends BaseISpec
+      with BeforeAndAfterAll
+      with ScalaFutures
+      with JsonMatchers
+      with WSResponseMatchers
+      with MongoDB
+      with IntegrationPatience {
 
   val playServer: TestPlayServer = TestPlayServer
   def port: Int = playServer.port
@@ -27,8 +33,5 @@ abstract class ServerBaseISpec
   implicit val defaultTimeout: FiniteDuration = 30.seconds
 
   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.global
-
-  implicit override val patienceConfig: PatienceConfig =
-    PatienceConfig(timeout = Span(15, Seconds), interval = Span(50, Milliseconds))
 
 }
