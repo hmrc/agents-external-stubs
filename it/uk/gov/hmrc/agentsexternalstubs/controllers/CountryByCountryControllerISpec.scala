52f1eab7058bbf99be96d2ca85b8e88fcca4b030
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-06-21 15:41:56 +0100

    fix [CS] update patience config add CbcISpec

---
+++ it/uk/gov/hmrc/agentsexternalstubs/controllers/CountryByCountryControllerISpec.scala
@@ -0,0 +1,96 @@
+package uk.gov.hmrc.agentsexternalstubs.controllers
+
+import play.api.libs.json.{JsBoolean, JsObject, Json}
+import play.api.libs.ws.WSClient
+import uk.gov.hmrc.agentsexternalstubs.models.{AuthenticatedSession, CbCRequestCommon, CbCRequestDetail, CbCResponseCommon, CbCResponseDetail, CbcSubscriptionRecord, DisplaySubscriptionForCbC, DisplaySubscriptionForCbCRequest, DisplaySubscriptionForCbCResponse}
+import uk.gov.hmrc.agentsexternalstubs.support._
+
+import java.time.LocalDateTime
+
+class CountryByCountryControllerISpec extends ServerBaseISpec with TestRequests {
+
+  lazy val wsClient: WSClient = app.injector.instanceOf[WSClient]
+  val cbcId = "XACBC1234567890"
+
+  "CountryByCountryController" when {
+
+    "POST /dac/dct50d/v1 (display cbc subscription) " should {
+      "respond 200 with country by country subscription details" in {
+        implicit val session: AuthenticatedSession = SignIn.signInAndGetSession()
+        val email = "test@foo.uk"
+
+        //gen a record
+        val createRecord = Records.createCbcSubscriptionRecord(
+          CbcSubscriptionRecord
+            .seed("foo")
+            .withCbcId(cbcId)
+            .withEmail(email)
+        )
+        createRecord should haveStatus(201)
+
+        val result = post[DisplaySubscriptionForCbCRequest](
+          s"/dac/dct50d/v1",
+          DisplaySubscriptionForCbCRequest(
+            CbCRequestCommon("CbC", None, LocalDateTime.now()),
+            CbCRequestDetail(IDNumber = cbcId)
+          )
+        )
+
+        result should haveStatus(200)
+        val json = result.json
+        json.as[JsObject] should haveProperty[JsObject]("displaySubscriptionForCbCResponse")
+        val responseCommon = (result.json \ "displaySubscriptionForCbCResponse" \ "responseCommon").as[JsObject]
+        responseCommon should haveProperty[String]("status", be("OK"))
+        responseCommon should haveProperty[LocalDateTime]("processingDate")
+        val responseDetail = (result.json \ "displaySubscriptionForCbCResponse" \ "responseDetail").as[JsObject]
+        responseDetail should haveProperty[String]("cbcId", be(cbcId))
+        responseDetail should haveProperty[JsBoolean]("isGBUser")
+        val primaryContact = (responseDetail \ "primaryContact").as[JsObject]
+        primaryContact should haveProperty[String]("email", be(email))
+      }
+
+      "respond NOT_FOUND with error response if no record" in {
+        implicit val session: AuthenticatedSession = SignIn.signInAndGetSession()
+
+        val result = post[DisplaySubscriptionForCbCRequest](
+          s"/dac/dct50d/v1",
+          DisplaySubscriptionForCbCRequest(
+            CbCRequestCommon("CbC", None, LocalDateTime.now()),
+            CbCRequestDetail(IDNumber = cbcId)
+          )
+        )
+
+        result should haveStatus(404)
+        val json = result.json
+        json.as[JsObject] should haveProperty[JsObject]("displaySubscriptionForCbCResponse")
+        val responseCommon = (result.json \ "displaySubscriptionForCbCResponse" \ "responseCommon").as[JsObject]
+        responseCommon should haveProperty[String]("status", be("NOT_OK"))
+        responseCommon should haveProperty[LocalDateTime]("processingDate")
+        val errorDetail = (result.json \ "displaySubscriptionForCbCResponse" \ "errorDetail").as[JsObject]
+        errorDetail should haveProperty[String]("errorCode", be("404"))
+        errorDetail should haveProperty[String]("errorMessage", be("Record not found"))
+      }
+
+      "respond BAD_REQUEST with error response if invalid payload" in {
+        implicit val session: AuthenticatedSession = SignIn.signInAndGetSession()
+
+        val result = post[DisplaySubscriptionForCbCRequest](s"/dac/dct50d/v1", DisplaySubscriptionForCbCRequest(
+          CbCRequestCommon("bad payload", None, LocalDateTime.now()),
+          CbCRequestDetail(IDNumber = "foo")
+        ))
+
+        result should haveStatus(400)
+        val json = result.json
+        json.as[JsObject] should haveProperty[JsObject]("displaySubscriptionForCbCResponse")
+        val responseCommon = (result.json \ "displaySubscriptionForCbCResponse" \ "responseCommon").as[JsObject]
+        responseCommon should haveProperty[String]("status", be("NOT_OK"))
+        responseCommon should haveProperty[LocalDateTime]("processingDate")
+        val errorDetail = (result.json \ "displaySubscriptionForCbCResponse" \ "errorDetail").as[JsObject]
+        errorDetail should haveProperty[String]("errorCode", be("400"))
+        errorDetail should haveProperty[String]("errorMessage", be("Invalid JSON document"))
+      }
+
+    }
+
+  }
+}
--- it/uk/gov/hmrc/agentsexternalstubs/support/ServerBaseISpec.scala
+++ it/uk/gov/hmrc/agentsexternalstubs/support/ServerBaseISpec.scala
@@ -2,7 +2,7 @@ package uk.gov.hmrc.agentsexternalstubs.support
 
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
-import org.scalatest.time.{Milliseconds, Seconds, Span}
+import org.scalatest.time.{Millis, Seconds, Span}
 import play.api.Application
 
 import scala.concurrent.ExecutionContext
@@ -28,5 +28,8 @@ abstract class ServerBaseISpec
   implicit val defaultTimeout: FiniteDuration = 30.seconds
 
   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.global
+
+  override val patienceConfig: PatienceConfig =
+    PatienceConfig(timeout = scaled(Span(20, Seconds)), interval = scaled(Span(150, Millis)))
 
 }
