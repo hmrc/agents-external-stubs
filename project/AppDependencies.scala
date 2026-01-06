b3a6e833743e6d6b6a21a322bb88b1163cefea9d
Iyad Issa <237692191+iscalit@users.noreply.github.com>
2026-01-06 09:04:34 +0000

    APB-10556: Bump bootstrap/mongo libs; keep stub-data-generator at 1.4.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,9 +2,9 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVer = "10.3.0"
+  private val bootstrapVer = "10.5.0"
 
-  private val mongoVer = "2.10.0"
+  private val mongoVer = "2.11.0"
 
   private val playVer = "play-30"
 
@@ -14,7 +14,9 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "domain-play-30"              % "11.0.0",
     "com.github.blemale"   %% "scaffeine"                   % "5.3.0",
     "org.typelevel"        %% "cats-core"                   % "2.13.0",
-    "uk.gov.hmrc"          %% "stub-data-generator"         % "1.4.0", // cannot update without moving to Scala 3
+    // NOTE: 1.6.0 exists in some environments but is not resolvable from this local build;
+    // we keep 1.4.0 here to preserve a working compile while still depending on the library at runtime.
+    "uk.gov.hmrc"          %% "stub-data-generator"         % "1.4.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"       % "0.1.3",
     "org.playframework"    %% "play-json"                   % "3.0.5"
   )
