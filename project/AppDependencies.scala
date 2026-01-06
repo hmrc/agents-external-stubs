a9fb889d75ec40fab495d1b46542dd65e6568b03
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2026-01-06 14:33:25 +0000

    APB-10289: Formatting updates to dependencies/plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,23 +2,21 @@ import sbt.*
 
 object AppDependencies {
 
+  private val playVer = "play-30"
+
   private val bootstrapVer = "10.5.0"
 
   private val mongoVer = "2.12.0"
 
-  private val playVer = "play-30"
-
   lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% s"bootstrap-backend-$playVer" % bootstrapVer,
     "uk.gov.hmrc.mongo"    %% s"hmrc-mongo-$playVer"        % mongoVer,
-    "uk.gov.hmrc"          %% "domain-play-30"              % "11.0.0",
-    "com.github.blemale"   %% "scaffeine"                   % "5.3.0",
+    "uk.gov.hmrc"          %% s"domain-$playVer"            % "11.0.0",
+    "uk.gov.hmrc"          %% "stub-data-generator"         % "1.4.0", // cannot update without moving to Scala 3
+    "org.playframework"    %% "play-json"                   % "3.0.6",
     "org.typelevel"        %% "cats-core"                   % "2.13.0",
-    // NOTE: 1.6.0 exists in some environments but is not resolvable from this local build;
-    // we keep 1.4.0 here to preserve a working compile while still depending on the library at runtime.
-    "uk.gov.hmrc"          %% "stub-data-generator"         % "1.4.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"       % "0.1.3",
-    "org.playframework"    %% "play-json"                   % "3.0.6"
+    "com.github.blemale"   %% "scaffeine"                   % "5.3.0"
   )
 
   lazy val test: Seq[ModuleID] = Seq(
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,8 +2,8 @@ resolvers += "HMRC-open-artefacts-maven" at "https://open.artefacts.tax.service.
 resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
 resolvers += Resolver.typesafeRepo("releases")
 
-addSbtPlugin("uk.gov.hmrc"       % "sbt-auto-build"        % "3.24.0")
 addSbtPlugin("org.playframework" % "sbt-plugin"            % "3.0.9")
+addSbtPlugin("uk.gov.hmrc"       % "sbt-auto-build"        % "3.24.0")
 addSbtPlugin("uk.gov.hmrc"       % "sbt-distributables"    % "2.6.0")
 addSbtPlugin("org.scoverage"     % "sbt-scoverage"         % "2.3.1")
 addSbtPlugin("org.scalameta"     % "sbt-scalafmt"          % "2.5.2")
