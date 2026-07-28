3a5fe24ef1c6e098ff5e1d36eb9e45b4363d15ca
Drew Miley <13115186+drewmiley@users.noreply.github.com>
2026-07-28 13:10:11 +0100

    APB-11839: Updates hosts so that they work will all domains for email generation

--- app/uk/gov/hmrc/agentsexternalstubs/models/Generator.scala
+++ app/uk/gov/hmrc/agentsexternalstubs/models/Generator.scala
@@ -188,7 +188,7 @@ object Generator
 
   lazy val emailGen: Gen[String] = for {
     domain       <- Gen.oneOf(".com", ".co.uk", ".uk", ".eu", ".me")
-    host         <- Gen.oneOf("mail", "post", "email", "inbox", "domain")
+    host         <- Gen.oneOf("imail", "post", "send", "inbox", "domain")
     size         <- Gen.chooseNum[Int](10, 32 - (domain.length + host.length))
     usernameSize <- Gen.chooseNum[Int](1, size - 3)
     username     <- stringMaxN(usernameSize)
