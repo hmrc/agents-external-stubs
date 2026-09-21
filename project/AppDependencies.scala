38e18eae24e2d50a584b664a7e8e0d5255898e47
Drew Miley <13115186+drewmiley@users.noreply.github.com>
2026-09-21 12:10:47 +0100

    APB-12191: Some initial version upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,13 @@ import sbt.*
 object AppDependencies {
 
   private val playVer = "play-30"
-
-  private val bootstrapVer = "10.7.1"
-
+  private val bootstrapVer = "10.8.0"
   private val mongoVer = "2.13.0"
 
   lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% s"bootstrap-backend-$playVer" % bootstrapVer,
     "uk.gov.hmrc.mongo"    %% s"hmrc-mongo-$playVer"        % mongoVer,
-    "uk.gov.hmrc"          %% s"domain-$playVer"            % "11.0.0",
+    "uk.gov.hmrc"          %% s"domain-$playVer"            % "13.0.0",
     "uk.gov.hmrc"          %% "stub-data-generator"         % "1.6.0",
     "org.playframework"    %% "play-json"                   % "3.0.6",
     "org.typelevel"        %% "cats-core"                   % "2.13.0",
